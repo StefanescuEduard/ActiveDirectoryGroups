572e194adb3837b24b72f0e63cef2290987909bc
Eduard Stefanescu <eduard.traian.stefanescu@gmail.com>
2021-10-10 15:32:19 +0300

    Renamed controller to ActiveDirectoryController to be more REST oriented.

---
+++ ActiveDirectoryGroups/Controllers/ActiveDirectoryController.cs
@@ -0,0 +1,22 @@
+﻿using ActiveDirectoryGroups.Services;
+
+namespace ActiveDirectoryGroups.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActiveDirectoryController : ControllerBase
+    {
+        private readonly ActiveDirectoryGroupsService activeDirectoryGroupsService;
+
+        public ActiveDirectoryController(ActiveDirectoryGroupsService activeDirectoryGroupsService)
+        {
+            this.activeDirectoryGroupsService = activeDirectoryGroupsService;
+        }
+
+        [HttpGet("{username}/groupNames")]
+        public IActionResult GetGroupNames(string username)
+        {
+            IEnumerable<string> groupNames = activeDirectoryGroupsService.GetGroupNames(username);
+            return Ok(groupNames);
+        }
+    }
+}
